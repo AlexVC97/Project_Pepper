2d3fd70491a3bb9ef3c0896adcd5edcda93edb7f
Alex <ussviper97@gmail.com>
2018-02-27 13:45:03 +0100

    Work in progress

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 #from rpi_broadcast import RpiBroadcast
 from nfc import Nfc
-import signal
-
-continue_reading = True
-
-# Capture SIGINT for cleanup when the script is aborted
-def end_read(signal,frame):
-    #global continue_reading
-    print "Ctrl+C captured, ending read."
-    myThreadOb2.continue_reading = False
-    GPIO.cleanup()
 
 if __name__ == "__main__":
     #myThreadOb1 = RpiBroadcast("1254268ECHDN")
     myThreadOb2 = Nfc()
-
-    # Hook the SIGINT
-    signal.signal(signal.SIGINT, end_read)
 
     #myThreadOb1.start()
     myThreadOb2.start()
