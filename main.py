9393fac47c980c265a0e74da892c83b381a8ae18
Alex <ussviper97@gmail.com>
2018-02-28 16:44:24 +0100

    Work in progress

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from broadcast import Broadcast
 from nfc import Nfc
 from time import *
 import json
-SerialNo = ""
+
+serialNo = ""
+
 with open('config.json') as json_data:
     data = json.load(json_data)
-    SerialNo = data['SerialNo']
-    print data['RFID']
+    serialNo = data['SerialNo']
+    rfid = data['RFID']
 
 if __name__ == "__main__":
-    print SerialNo
-    broadcast = Broadcast("1254268ECHDN")
+    broadcast = Broadcast(serialNo)
     nfcThread = Nfc()
 
     broadcast.config_socket()
