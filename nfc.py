f39d4e2c3a6cb0f055dca8920c844412a7e1826a
Alex <ussviper97@gmail.com>
2018-03-02 14:50:07 +0100

    Work in progress

--- nfc.py
+++ nfc.py
@@ -19,40 +19,39 @@ logger.addHandler(file_handler)
 class Nfc(Thread):
     def __init__(self):
         Thread.__init__(self)
-        self.continue_reading = True
-        self.uid = [0,0,0,0,0]
-        # Create an object of the class MFRC522
-        self.MIFAREReader = MFRC522.MFRC522()
 
     def run(self):
         oldUid = [0,0,0,0,0]
+
+        # Create an object of the class MFRC522
+        MIFAREReader = MFRC522.MFRC522()
 
         # Welcome message
         print "Identificatie APP"
         print "Press break to stop."
 
-        self.MIFAREReader.Write_MFRC522(0x04,0x55)
+        MIFAREReader.Write_MFRC522(0x04,0x55)
 
         # This loop keeps checking for chips. If one is near it will get the UID and authenticate
-        while self.continue_reading:
+        while continue_reading:
             # Scan for cards
-            (status,TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
+            (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
             sleep(1)
             # If a card is found
-            if status == self.MIFAREReader.MI_OK:
+            if status == MIFAREReader.MI_OK:
                 #print "Card detected"
                 # Get the UID of the card
-                (status,self.uid) = self.MIFAREReader.MFRC522_Anticoll()
+                (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
                 # If we have the UID, continue
-                if status == self.MIFAREReader.MI_OK:
+                if status == MIFAREReader.MI_OK:
                     #Print UID
                     #print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])+","+str(uid[4])
 
                     flag_read = True
-                    if oldUid != self.uid:
-                        oldUid = self.uid
-                        logger.debug("Card read UID: " + str(self.uid))
+                    if oldUid != uid:
+                        oldUid = uid
+                        logger.debug("Card read UID: " + str(uid))
                        # This is the default key for authentication
                         key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
