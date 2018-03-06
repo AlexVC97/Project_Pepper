a48e0b1a3d595331b2a37c61bc22bb511d619af8
Alex <ussviper97@gmail.com>
2018-03-06 15:57:36 +0100

    Work in progress

--- client_handler.py
+++ client_handler.py
@@ -5,27 +5,27 @@ class ClientHandler():
     def __init__(self, broker_ip, broker_port):
         self.broker_ip = broker_ip
         self.broker_port = broker_port
+        self.publish = Publish()
+        self.client = mqtt.Client("PUB_CLIENT") # Create new instance
 
     def make_connection(self):
-        publish = Publish()
         mqtt.Client.connected_flag = False # Create flag in class
-        client = mqtt.Client("PUB_CLIENT") # Create new instance
-        client.on_log = publish.on_log
-        client.on_connect = publish.on_connect
-        client.on_disconnect = publish.on_disconnect
-        client.on_publish = publish.on_publish
-        client.connect(self.broker_ip, self.broker_port) # Establish connection
-        client.loop_start()
-        while not client.connected_flag: # Wait in loop
+        self.client.on_log = self.publish.on_log
+        self.client.on_connect = self.publish.on_connect
+        self.client.on_disconnect = self.publish.on_disconnect
+        self.client.on_publish = self.publish.on_publish
+        self.client.connect(self.broker_ip, self.broker_port) # Establish connection
+        self.client.loop_start()
+        while not self.client.connected_flag: # Wait in loop
             print "In wait loop"
             time.sleep(1)
         time.sleep(3)
 
     def publish(self, topic, msg):
-        ret = client.publish(topic, msg, 0) # Publish
+        ret = self.client.publish(topic, msg, 0) # Publish
         print "Published return = " + str(ret)
         time.sleep(3)
 
     def disconnect(self):
-        client.loop_stop()
-        client.disconnect()
+        self.client.loop_stop()
+        self.client.disconnect()
