a2546c166c495f7955cfd4226510d6dfff3e3dd7
Alex <ussviper97@gmail.com>
2018-03-07 11:45:10 +0100

    Work in progress

--- communication.py
+++ communication.py
@@ -3,19 +3,16 @@ import time
 import logging
 
 class Communication():
-    def on_log(self, client, userdata, level, buf):
-        logging.info(buf)
-
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
             client.connected_flag = True # Set flag
-            logging.info("connected OK")
+            logging.info("communication:Client connected OK")
         else:
-            logging.info("bad connection returned code = " + str(rc))
+            logging.info("communication:Bad connection returned code = " + str(rc))
             client.loop_stop()
 
     def on_disconnect(self, client, userdata, rc):
-        logging.info("client disconnected OK")
+        logging.info("communication:Client disconnected OK")
 
     def on_publish(self, client, userdata, mid):
-        logging.info("in on_pub callback mid = " + str(mid))
+        logging.info("communication:in on_pub callback mid = " + str(mid))
